ce21091756bdb98ddfdde6652e610d51d0e79de2
Ruslan Feduleyev <8reflections@gmail.com>
2024-05-04 04:26:02 +0500

    READY

--- modals/index.js
+++ modals/index.js
@@ -1,3 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log(1);
+    const modalButtons = document.querySelectorAll('[data-modal-button]')
+    const modalWrapper = document.querySelector('.modalWrapper')
+
+    function hideAllModals() {
+        const modals = document.querySelectorAll('[data-modal]')
+        modals.forEach(modal => {
+            modal.classList.add('modalHidden')
+        })
+    }
+
+    function hideModalWrapper() {
+        modalWrapper.classList.add('modalWrapper--Hidden')
+    }
+
+    function showModalWrapper() {
+        modalWrapper.classList.remove('modalWrapper--Hidden')
+    }
+
+    modalButtons.forEach(btn => {
+        btn.addEventListener('click', e => {
+            showModalWrapper()
+            hideAllModals()
+
+            const thisAttribute = e.currentTarget.getAttribute('data-modal-button')
+            const thisModal = modalWrapper.querySelector(`[data-modal='${thisAttribute}']`)
+            thisModal?.classList?.remove('modalHidden')
+        })
+    })
+
+    /* Close by icon */
+    const closeIcons = document.querySelectorAll('.modalWrapper__woodBg-closeIcon')
+    closeIcons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            hideModalWrapper()
+            hideAllModals()
+        })
+    })
+
+    /* Close by click outside */
+    modalWrapper.addEventListener('click', e => {
+        if (!e.target.closest('.modalWrapper__content')) {
+            hideModalWrapper()
+            hideAllModals()
+        }
+    })
 })
