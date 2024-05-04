f44d439fbffaec2f2bf75a934394d5050a345092
Ruslan Feduleyev <8reflections@gmail.com>
2024-05-05 02:19:34 +0500

    FINAL

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const scales = document.querySelectorAll('[data-scale]')
-  scales.forEach(scale => {
-
-    getScaleValue(scale)
-
-    const observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        if (mutation.attributeName === 'data-scale-progress') {
-          getScaleValue(scale);
-        }
-      });
-    });
-
-    observer.observe(scale, { attributes: true });
-  })
-
+  // calculate document height
   const documentHeight = () => {
     const doc = document.documentElement
     doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
@@ -22,12 +7,73 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('resize', documentHeight)
   documentHeight()
 
+
+  const scaleContainer = document.querySelectorAll('.main__bar-wood-ScaleContainer')
+
+  scaleContainer.forEach(container => {
+    const scales = document.querySelectorAll('[data-scale]')
+    handleScaleContainerRender(container)
+
+    const childrenCountCallback = function(mutationsList, observer) {
+      for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          handleScaleContainerRender(container)
+        }
+      }
+    };
+
+    const observer = new MutationObserver(childrenCountCallback);
+    const config = { childList: true };
+    observer.observe(container, config);
+
+    scales.forEach((scale, index) => {
+      getScaleValue(scale)
+
+      const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+          if (mutation.attributeName === 'data-scale-progress') {
+            getScaleValue(scale);
+          }
+        });
+      });
+
+      observer.observe(scale, { attributes: true });
+    })
+
+  })
+
+
+
   function getScaleValue(scale) {
     const value = scale.getAttribute('data-scale-progress')
-    const result = (value / 100) * 210
-    scale.setAttribute('style', `height: ${result}px`)
+    scale.setAttribute('style', `height: ${value}px`)
   }
 
+  function handleScaleContainerRender(container) {
+    const count = container.childElementCount
+    const items = []
+    container.childNodes.forEach(item => {
+      if (item.tagName === 'DIV') {
+        items.push(item)
+      }
+    })
+    items.forEach((item, i) => {
+      const isSecondColumnItem = (i + 1) - 5 > 0
+      if (isSecondColumnItem) {
+         item.setAttribute('style', `grid-row: ${5 + (5 - i)}`)
+      } else {
+        item.setAttribute('style', `grid-row: ${5 - i}`)
+      }
+    })
+
+    if (count > 5) {
+      container.classList.add('main__bar-wood-ScaleContainer--TwoColumns')
+    }
+
+    if (count <= 5) {
+      container.classList.remove('main__bar-wood-ScaleContainer--TwoColumns')
+    }
+  }
 })
 
 
